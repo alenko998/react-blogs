2a54bff3bf08d6d233dea30dfa03b282d4d7aa66
alenko998 <alen_git@hotmail.com>
2024-01-03 17:48:51 +0100

    navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>React Blog App</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blog</a>
+      </div>
+    </div>
+  )
+}
